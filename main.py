111027ab36d0e26b26ea3c891a3acd164adbdb5b
kamiprok <kamiprok@gmail.com>
2018-12-03 15:20:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -46,54 +46,72 @@ def menu():
         menu()
     if x == '3':
         os.system('cls')
+
         def horner():
             powtorz = 0
             while powtorz == 0:
                 try:
                     n = int(input('podaj stopien wielomianu: \n'))
                 except ValueError:
-                    print('To nie jest liczba')
+                    print('To nie jest liczba!')
                     horner()
                 else:
-                    if n > 0 and n < 21:
+                    if n > 1 and n < 21:
                         print('wybrano :', n)
                         y = n
 
                         tablica = []
                         for x in range(n + 1):
-                            print('podaj liczbe przy potedze x ^', y)
-                            z = int(input())
-                            tablica.append(z)
-                            y = y - 1
+                            try:
+                                print('podaj liczbe przy potedze x ^', y)
+                                z = int(input())
+                            except ValueError:
+                                print('To nie jest liczba!!')
+                            else:
+                                if z > -1000000 and z < 1000000:
+                                    tablica.append(z)
+                                    y = y - 1
+                                else:
+                                    print('podaj inną liczbę...')
                         print(tablica)
 
-                        print('podaj c: (x - c) ')
-                        c = int(input())
-                        print('wybrano c =', c)
+                        try:
+                            print('podaj c: (x - c) ')
+                            c = int(input())
+                        except ValueError:
+                            print('To nie jest liczba!!!')
+                        else:
+                            if c > -1000000 and c < 1000000:
+                                if c != 0:
+                                    print('wybrano c =', c)
 
-                        i = len(tablica)
-                        print('dlugosc tablicy:', i)
+                                    i = len(tablica)
+                                    print('dlugosc tablicy:', i)
 
-                        tablica2 = []
-                        tablica2.append(tablica[0])
+                                    tablica2 = []
+                                    tablica2.append(tablica[0])
 
-                        for i in range(1, n + 1):
-                            tablica2.append(tablica2[i - 1] * c + tablica[i])
-                        print(tablica2)
+                                    for i in range(1, n + 1):
+                                        tablica2.append(tablica2[i - 1] * c + tablica[i])
+                                    print(tablica2)
 
-                        m = n - 1
+                                    m = n - 1
 
-                        print('Wynik: ')
-                        print('( x -', c, ')( ', end="")
-                        for i in range(1, n):
-                            print(tablica2[i - 1], 'x ^', m, ' + ', end="")
-                            m = m - 1
-                        print(tablica2[i], "} i ", tablica2[i + 1], ' reszty.')
-                        print('\n### KONIEC ###\n')
-                        if input('Jeszcze raz?  [y/n]: ') == 'n':
-                            powtorz = 1
+                                    print('Wynik: ')
+                                    print('( x -', c, ')( ', end="")
+                                    for i in range(1, n):
+                                        print(tablica2[i - 1], 'x ^', m, ' + ', end="")
+                                        m = m - 1
+                                    print(tablica2[i], "} i ", tablica2[i + 1], ' reszty.')
+                                    print('\n### KONIEC ###\n')
+                                    if input('Jeszcze raz?  [y/n]: ') == 'n':
+                                        powtorz = 1
+                                else:
+                                    print('c nie może być 0 bo to nie będzie dwumian')
+                            else:
+                                print('wybierz inną liczbę...')
                     else:
-                        print('podaj liczbę z przedziału (1 - 20)')
+                        print('podaj liczbę z przedziału (2 - 20)')
                         horner()
 
         horner()
